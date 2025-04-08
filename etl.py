98d30b4e1f34cc4a7a1e26df0d1fdbcc04e25eaf
solv <solvasquez22@hotmail.com>
2025-04-07 21:48:40 -0500

    commit inicial proyect 03

--- etl.py
+++ etl.py
@@ -2,6 +2,6 @@
 import pandas as pd
 
 df = pd.read_csv('input.csv')
-df['processed'] = df['value'] * 1000
+df['processed'] = df['value'] * 10
 df.to_csv('output4.csv', index=False)
 print("ETL completado.")
